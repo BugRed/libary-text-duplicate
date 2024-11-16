493fa098f359235ddfca5a164cd1c3fb161d664c
BugRed <o.adrian.rodrigues@gmail.com>
2024-11-16 11:21:43 -0300

    Criação das funções de contagem e separação de palavras e paragrafos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ const fs = require('fs');
 const filePath = process.argv;
 const link = filePath[2];
 
-fs.readFile(link, 'utf8', (err, text) =>{
-    console.log(text);
+fs.readFile(link, 'utf8', (err, fileText) => {
+    breakParagraphCount(fileText)
 });
+
+
+const breakParagraphCount = (text) => {
+    const paragraph = text.toLowerCase().split('\n');
+    const count = paragraph.flatMap((paragraph)=> {
+        if(!paragraph) return [];
+        return countDuplicateWord(paragraph);
+    })
+    console.log(count);
+    
+}
+
+const cleanCharacter = (word) => {
+    return word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\r]/g, '');
+}
+
+const countDuplicateWord = (text) => {
+    const wordList = text.split(' ');
+    const result = {};
+
+    wordList.forEach(word => {
+
+        if (word.length >= 3) {
+            const cleanWord = cleanCharacter(word);
+            result[cleanWord] = (result[cleanWord] || 0) + 1;
+        }
+
+    });
+    return result;
+}
---
+++ test.js
@@ -0,0 +1,10 @@
+const paragrafos = ["código", "js", "", "web", "", "array"];
+
+const result = paragrafos.reduce((acum, paragrafo) => {
+ if (paragrafo) {
+   return [...acum, paragrafo];
+ }
+ return acum;
+}, []);
+
+console.log(result);
